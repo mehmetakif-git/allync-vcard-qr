484c4602dd253517d336d97f9f42c4e66bd5b3e4
mehmetakif-git <mehmetakifaybey@icloud.com>
2025-10-21 11:06:37 +0300

    fix: shiny text paddin issue

--- src/components/ShinyText.jsx
+++ src/components/ShinyText.jsx
@@ -8,7 +8,10 @@ const ShinyText = ({ text, className = '', speed = 5 }) => {
         backgroundImage: 'linear-gradient(120deg, rgba(255,255,255,0.5) 0%, rgba(255,255,255,1) 50%, rgba(255,255,255,0.5) 100%)',
         backgroundSize: '200% 100%',
         WebkitBackgroundClip: 'text',
-        animation: `shine ${speed}s linear infinite`
+        animation: `shine ${speed}s linear infinite`,
+        paddingLeft: '0.1em',
+        paddingRight: '0.1em',
+        overflow: 'visible'
       }}
     >
       {text}
